a837154dc36fe53e345a2fb13a42a496eeefff0a
Dinh Long <dinhlong366@gmail.com>
2025-08-01 16:50:13 +0700

    test chat

--- client/src/pages/Chats/ChatApp.tsx
+++ client/src/pages/Chats/ChatApp.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useSocket } from '../../hooks/useSocket'
+import { Button } from '../../components/ui/Button'
 
 const ChatApp = () => {
+  const { connected, error, reconnecting } = useSelector((state: any) => state.socket);
+  const { socketService } = useSocket();
+
+  if (socketService.isConnected) {
+    console.log('Socket is connected');
+  } else {
+    console.log('Socket is not connected');
+  }
+
   return (
     <div>
       aloalo
-    </div>
+      <Button onClick={() => socketService.createConversation({
+        type: 'direct',
+        participantIds: ['684fa424cb6767c7256a1739'],
+        name: 'New Conversation',
+      })}>
+        Send Message
+      </Button>
+      <Button onClick={() => socketService.sendMessage({
+        conversationId: '688c8d3a1768bb59a3d98896',
+        content: 'Hello, world!',
+        type: 'text',
+      })}>
+      Send
+    </Button>
+    </div >
   )
 }
 
--- client/src/pages/Chats/ChatWindow.tsx
+++ client/src/pages/Chats/ChatWindow.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { Input } from '../../components/ui/Input'
 import { Button } from '../../components/ui/Button'
+import { useSelector } from 'react-redux'
+import { useSocket } from '../../hooks/useSocket'
 
 const ChatWindow = () => {
+  const { connected, error, reconnecting } = useSelector((state: any) => state.socket);
+  const { socketService } = useSocket();
+
+  if (socketService.isConnected) {
+    console.log('Socket is connected');
+  } else {
+    console.log('Socket is not connected');
+  }
+
   return (
     <div>
       
